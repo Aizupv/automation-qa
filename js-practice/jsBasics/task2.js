4f68bbef89a153d78ca44b779baf1e64d31129f9
Aizupv <aizupv@gmail.com>
2025-12-11 15:53:33 +0200

    Add folder jsBasics and   files : task1.js , task2.js , task3.js, task4.js

---
+++ js-practice/jsBasics/task2.js
@@ -0,0 +1,11 @@
+let name = 'John';
+let surname = 'Pitersen';
+let fullName = name + surname;
+console.log(fullName);
+console.log(name + surname);
+
+// Шаблонний рядок 
+let greetings = `Hi, glad to see You mr. ${fullName}`;
+let greetings1 = `Hello, ${name} ${surname}`;
+console.log(greetings);
+console.log(greetings1);
