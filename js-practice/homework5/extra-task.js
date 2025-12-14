b7f337853851e53e7e70e418c2938c7d5a9f9d35
Aizupv <aizupv@gmail.com>
2025-12-14 20:37:25 +0200

    Add extra-task.js file to the homework5-branch

---
+++ js-practice/homework5/extra-task.js
@@ -0,0 +1,10 @@
+const string = 'ReWFDAFDSF';
+let a='';
+for (let i = 0 ; i<=string.length -1  ; i++ ){
+ a += string[i];
+}
+ if(a === a.toUpperCase()){
+   console.log(true);
+ } else {
+    console.log(false);
+}
--- js-practice/homework5/task2.js
+++ js-practice/homework5/task2.js
@@ -12,7 +12,7 @@ case (averageGrade >= 71 && averageGrade <=80) :
 case (averageGrade >= 81 && averageGrade <=100) :
     console.log('Відмінно');
     break;
-case (averageGrade >= 100 || averageGrade <0 || typeof(addEventListener)!= Number ) :
+case (averageGrade >= 100 || averageGrade <0 || typeof(averageGrade)!= Number ) :
     console.log('Значення некорректне');
     break;
 
