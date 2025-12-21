c8ba3d45c8e5aad744b39cb37d1a4d49646a2aed
Aizupv <aizupv@gmail.com>
2025-12-21 12:34:11 +0200

    Update files with tasks

--- js-practice/homework6/task2.js
+++ js-practice/homework6/task2.js
@@ -1,13 +1,13 @@
-function isAdult (age){
-    const notAdult = 'The person isn`t adult' ;
-    const isAdult = 'The person is adult' ;
-    if (age < 18){
+function isAdult(age) {
+    const notAdult = 'The person isn`t adult';
+    const isAdult = 'The person is adult';
+    if (age < 18) {
         return notAdult
     }
     else if (age >= 18 && age <= 100) {
-        return isAdult
+        return isAdult;
     }
-    return 'Wrong input'
+    return 'Wrong input';
 }
-console.log(isAdult(15)) 
+console.log(isAdult(15))
 console.log(isAdult(25))
--- js-practice/homework6/task3.js
+++ js-practice/homework6/task3.js
@@ -1,16 +1,16 @@
 
 
-function checkOrder(available, ordered){
-    const noGoodsMessage = 'Your order is too large, we don’t have enough goods.' ;
-    const orderEmptyMessage = 'Your order is empty' ;
-    const acceptedOrderMessage = 'Your order is accepted' ;
-    if (available < ordered){
+function checkOrder(available, ordered) {
+    const noGoodsMessage = 'Your order is too large, we don’t have enough goods.';
+    const orderEmptyMessage = 'Your order is empty';
+    const acceptedOrderMessage = 'Your order is accepted';
+    if (available < ordered) {
         return noGoodsMessage
     }
-    else if(ordered === 0 ){
+    else if (ordered === 0) {
         return orderEmptyMessage
     }
     else return acceptedOrderMessage
-        
+
 }
-console.log(checkOrder(40 ,5))
+console.log(checkOrder(40, 5))
