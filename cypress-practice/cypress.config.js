877ba5f33357e0897de51d6592edd498dc588228
Aizupv <aizupv@gmail.com>
2026-03-15 13:15:33 +0200

    All changes for preparing tto switch for playwrite

--- cypress-practice/cypress.config.js
+++ cypress-practice/cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	allowCypressEnv: true,
+	watchForFileChanges: false,
 
 	e2e: {
 		baseUrl: `https://guest:welcome2qauto@qauto.forstudy.space`,
